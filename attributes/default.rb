58a300f15038f3d5ef3e0c6b073530e258fd1331
EvilScott <reis.robert.s@gmail.com>
2014-10-26 17:06:36 -0700

    remove init.d/unicorn (never worked reliably)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,8 @@
 default['nginx']['default_site_enabled'] = false
 default['nginx']['server_name'] = 'rails.dev'
+
 default['rails']['root'] = '/vagrant'
 default['rails']['env'] = 'development'
+
 default['unicorn']['sock_path'] = '/tmp/unicorn.sock'
+default['unicorn']['pid_path'] = '/vagrant/tmp/pids/unicorn.pid'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,35 +5,17 @@
 # Copyright 2014, Robert S. Reis
 #
 
-execute 'Bundler setup' do
+execute 'bundler' do
   command 'gem install bundler && bundler install'
   cwd node['rails']['root']
 end
 
+execute 'unicorn' do
+  command "unicorn_rails -c config/unicorn.rb -E #{node['rails']['env']} -D"
+  cwd node['rails']['root']
+end
+
 include_recipe 'nginx'
-
-cookbook_file 'unicorn.sh' do
-  path '/etc/init.d/unicorn'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
-
-service 'unicorn' do
-  supports start: true, stop: true, restart: true
-  action :nothing
-end
-
-directory '/etc/unicorn' do
-  recursive true
-end
-
-template '/etc/unicorn/rails.conf' do
-  source 'unicorn.conf.erb'
-  variables rails_env: node['rails']['env'],
-            rails_root: node['rails']['root']
-  notifies :start, 'service[unicorn]'
-end
 
 template '/etc/nginx/sites-enabled/rails.conf' do
   source 'nginx.conf.erb'
