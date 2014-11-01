98e48a9fed53a419b15e32cef2312820bc54132c
EvilScott <reis.robert.s@gmail.com>
2014-11-01 00:20:38 -0700

    try bringing back upstart for unicorn

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,33 @@
-#
 # Cookbook Name:: rails-unicorn-nginx
 # Recipe:: default
-#
 # Copyright 2014, Robert S. Reis
-#
 
-execute 'bundler' do
-  command 'gem install bundler && bundler install'
-  cwd node['rails']['root']
+gem_package 'bundler' do
+  notifies :run, 'execute[bundle]'
 end
 
-execute 'unicorn' do
-  command "unicorn_rails -c config/unicorn.rb -E #{node['rails']['env']} -D"
+execute 'bundle' do
   cwd node['rails']['root']
+  action :nothing
+end
+
+cookbook_file 'unicorn.sh' do
+  path '/etc/init.d/unicorn'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+service 'unicorn' do
+  supports start: true, stop: true, restart: true
+  action :nothing
+end
+
+template '/etc/unicorn/rails.conf' do
+  source 'unicorn.conf.erb'
+  variables rails_env: node['rails']['env'],
+            rails_root: node['rails']['root']
+  notifies :start, 'service[unicorn]'
 end
 
 include_recipe 'nginx'
