092255eb5b03629971951f1f0108453ec2afbbfd
EvilScott <reis.robert.s@gmail.com>
2014-11-01 19:51:14 -0700

    bring back unicorn upstart script as a template

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Copyright 2014, Robert S. Reis
 
 gem_package 'bundler' do
-  notifies :run, 'execute[bundle]'
+  notifies :run, 'execute[bundle]', :immediately
 end
 
 execute 'bundle' do
@@ -11,23 +11,19 @@ execute 'bundle' do
   action :nothing
 end
 
-cookbook_file 'unicorn.sh' do
-  path '/etc/init.d/unicorn'
+template '/etc/init.d/unicorn' do
+  source 'unicorn.erb'
   owner 'root'
   group 'root'
   mode '0755'
+  variables rails_env: node['rails']['env'],
+            rails_root: node['rails']['root'],
+            pid_path: node['unicorn']['pid_path']
 end
 
 service 'unicorn' do
   supports start: true, stop: true, restart: true
-  action :nothing
-end
-
-template '/etc/unicorn/rails.conf' do
-  source 'unicorn.conf.erb'
-  variables rails_env: node['rails']['env'],
-            rails_root: node['rails']['root']
-  notifies :start, 'service[unicorn]'
+  action [ :enable, :start ]
 end
 
 include_recipe 'nginx'
@@ -37,5 +33,5 @@ template '/etc/nginx/sites-enabled/rails.conf' do
   variables sock_path: node['unicorn']['sock_path'],
             rails_root: node['rails']['root'],
             server_name: node['nginx']['server_name']
-  notifies :start, 'service[nginx]'
+  notifies :restart, 'service[nginx]'
 end
